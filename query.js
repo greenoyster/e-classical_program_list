676ec567d68b8dd8b8a9679ab6fd5ed6e93f8def
Cheng-Yu Lee <greenoyster@gmail.com>
2019-02-04 04:10:35 +0800

    accept date as argument

--- query.js
+++ query.js
@@ -1,30 +1,8 @@
 const request = require('request-promise')
 const parser = require('node-html-parser');
 const decode = require('unescape');
-const datetime = require('node-datetime');
 const format = require('string-format')
-
-function main() {
-  var options = {
-        method: 'GET',
-        uri: makeUrl(),
-        headers: {referer: 'https://www.e-classical.com.tw/schedule_detail.html'},
-  };
-  request(options)
-    .then(function (response) {
-        const root = parser.parse(response);
-        const list = root.querySelector(".schedule_list");
-        const times = list.querySelectorAll(".schedule_list-time");
-        const details = list.querySelectorAll(".schedule_list-detail");
-        for (var i=0; i < times.length; ++i) {
-          printNodes(times[i], details[i]);
-          console.log();
-        }
-    })
-    .catch(function (err) {
-      // Something bad happened, handle the error
-    })
-}
+const date = require('date-and-time');
 
 function unescapeHTML(text) {
   return decode(text).replace(/&nbsp;/g, "");
@@ -44,25 +22,90 @@ function printNodes() {
   console.log(unescapeHTML(out.join('\n')));
 }
 
-function makeArgs() {
-  var now = datetime.create();
-  var date = now.format('Y-m-d');
-  var time = now.format('H:M');
-  return args = {
+function makeUrl(time) {
+  const baseURL = 'https://www.e-classical.com.tw/schedule_detail.html';
+  var args =  {
       radio_type: 1,
-      show_day: date,
-      current_time: time
+      show_day: date.format(time, 'YYYY-MM-DD'),
+      current_time: date.format(time, 'hh:mm'),
   };
+  var uriComponents = [];
+  for (key in args) {
+    uriComponents.push(key+'='+args[key])
+  }
+  return format('{0}?{1}', baseURL, encodeURI(uriComponents.join('&')));
 }
 
-function makeUrl() {
-  const baseURL = 'https://www.e-classical.com.tw/schedule_detail.html';
-  var components = [];
-  var args = makeArgs();
-  for (key in args) {
-    components.push(key+'='+args[key])
+function parseDateTime() {
+  time = new Date();
+  for (var i = 2; i < process.argv.length; ++i) {
+    var arg = process.argv[i];
+    var input = date.parse(arg, 'YYYYMMDD');
+    if (!isNaN(input)) {
+      time.setFullYear(input.getFullYear());
+      time.setMonth(input.getMonth());
+      time.setDate(input.getDate());
+      continue;
+    }
+
+    input = date.parse(arg, 'MMDD');
+    if (!isNaN(input)) {
+      time.setMonth(input.getMonth());
+      time.setDate(input.getDate());
+      continue;
+    }
+
+    input = date.parse(arg, 'H:m');
+    if (!isNaN(input)) {
+      time.setHours(input.getHours());
+      time.setMinutes(input.getMinutes());
+      continue;
+    }
+
+    input = date.parse(arg, 'H');
+    if (!isNaN(input)) {
+      time.setHours(input.getHours());
+      time.setMinutes(0);
+      continue;
+    }
+    console.error('Bad argument:', arg);
+    return null
   }
-  return format('{0}?{1}', baseURL, components.join('&'));
+  return time;
+}
+
+function main() {
+  var time = parseDateTime();
+  if (!time) {
+    console.error('Bad input, YYYYMMDD HH:mm or MMDD HH:mm');
+    return;
+  }
+  console.log(format('Querying schedule on {0}\n', date.format(time, 'YYYY-MM-DD HH:mm A')));
+  var options = {
+        method: 'GET',
+        uri: makeUrl(time),
+        headers: {
+          referer: 'https://www.e-classical.com.tw/schedule_detail.html'
+        },
+  };
+  request(options)
+    .then(function (response) {
+        const root = parser.parse(response);
+        const list = root.querySelector(".schedule_list");
+        if (!list) {
+          throw 'no content found';
+        }
+        const times = list.querySelectorAll(".schedule_list-time");
+        const details = list.querySelectorAll(".schedule_list-detail");
+        for (var i=0; i < times.length; ++i) {
+          printNodes(times[i], details[i]);
+          console.log();
+        }
+    })
+    .catch(function (err) {
+        console.log('Failed to make request to e-classical:', err);
+    })
 }
 
 main();
+
